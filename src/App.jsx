7c3ed88f40cbb51fce6c6fdf6e5b6b91f4c99b51
micf2f <micf2f@gmail.com>
2025-04-07 13:16:49 +0300

    Changed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+// import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout";
 import ErrorRoute from "./routes/ErrorRoute";
@@ -11,35 +12,48 @@ import CartRoute from "./routes/CartRoute";
 import "./App.css";
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorRoute />,
-    children: [
-      {
-        path: '/',
-        element: <SigninRoute />
-      },
-      {
-        path: '/books',
-        element: <ItemsListRoute />
-      },
-      {
-        path: '/books/:bookId',
-        element: <ItemRoute />
-      },
-      {
-        path: '/cart',
-        element: <CartRoute />
-      }
-    ]
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     errorElement: <ErrorRoute />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <SigninRoute />
+//       },
+//       {
+//         path: '/books',
+//         element: <ItemsListRoute />
+//       },
+//       {
+//         path: '/books/:bookId',
+//         element: <ItemRoute />
+//       },
+//       {
+//         path: '/cart',
+//         element: <CartRoute />
+//       }
+//     ]
+//   }
+// ]);
 
 
 function App() {
-  return <RouterProvider router={router} />
+  // return <RouterProvider router={router} />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SigninRoute />} />
+          <Route path="/books" element={<ItemsListRoute />} />
+          <Route path="/books/:bookId" element={<ItemRoute />} />
+          <Route path="/cart" element={<CartRoute />} />
+        </Route>
+        <Route path="*" element={<ErrorRoute />} />
+      </Routes>
+    </Router>
+  );
 }
 
 
